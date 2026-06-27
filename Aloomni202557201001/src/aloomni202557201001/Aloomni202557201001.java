1952eef8cd8f65a4e0dd33432c8df02b6a7bbe7d
achmad_khusnul_yakin <digivoraofficial81@gmail.com>
2026-06-27 08:32:04 +0700

    Upload Project Aplikasi Aloomni ke Github

---
+++ Aloomni202557201001/src/aloomni202557201001/Aloomni202557201001.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aloomni202557201001;
+
+/**
+ *
+ * @author achmad_khusnul_yakin
+ */
+public class Aloomni202557201001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
